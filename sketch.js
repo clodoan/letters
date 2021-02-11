d349b54d7a223785dcb865a8db503f53a16ed680
Claudio Angrigiani <angrigianicr@gmail.com>
2021-02-10 20:56:34 -0500

    centered canvas

--- sketch.js
+++ sketch.js
@@ -1,101 +1,53 @@
-var sketch1 = function(p) {
+var canvasWidth = 1600;
+var canvasHeight = 800;
+var lineHeight = 24;
+var sep = 16;
+var letW = 16;
+var t = 0;
+
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+}
+
+function mousePressed() {
+  redraw();
+}
+
+function createNoise(index) {
+  return 100/noise(index) - 10;
+}
+
+function draw(){
+  background(51);
+  t++;
+
+  for (i = 0, y = 0; i * sep < canvasWidth; i++) {
+    for (j = 0; j * lineHeight < canvasHeight; j++) {
+      //separation of words
+      if (random() > 0.3) {
+        x = i * sep;
+      } else {
+        x = i * sep + 80;
+      }
+        
+      y = j * lineHeight;
+      noFill();
+      stroke('#fff');
   
-  p.canvasWidth = 1600;
-  p.canvasHeight = 800;
-  p.lineHeight = 24;
-  p.sep = 16;
-  p.letW = 16;
-  p.t = 0;
-
-  p.setup = function() {
-    p.createCanvas(p.canvasWidth, p.canvasHeight);
-  }
-
-
-  p.createNoise = function(index) {
-    return 100/p.noise(index) - 10;
-  }
-
-  p.draw = function() {
-    p.background(51);
-    p.t++;
-
-    for (i = 0, y = 0; i * p.sep < p.canvasWidth; i++) {
-      for (j = 0; j * p.lineHeight < p.canvasHeight; j++) {
-        if (p.random() > 0.3) {
-          p.x = i * p.sep;
-        } else {
-          p.x = i * p.sep + 80;
-        }
-         
-        p.y = j * p.lineHeight;
-        p.noFill();
-        p.stroke('#fff');
-        if (p.random()<0.005) {
-          i += 10 * p.random();
-        }
-    
-        p.curve (
-          p.createNoise(i) + p.x,
-          p.createNoise(i * 300) + p.y,
-          p.x,
-          p.y + p.lineHeight * 2,
-          p.x - 16 * j / 24,
-          p.y + 40,
-          p.createNoise(j++ / 20) + p.x,
-          p.createNoise(j++) + p.y
-        )
-      }
-    }
-    p.noLoop();
-  }
-}
-
-var sketch2 = function(p) {
-  p.sep = 80;
-  p.canvasWidth = 1600;
-  p.canvasHeight = 800;
-  p.lineHeight = 30;
-  p.letW = 15;
-  p.t = 0;
-
-  p.setup = function() {
-    p.createCanvas(p.canvasWidth, p.canvasHeight);
-  }
-
-
-  p.createNoise = function(index) {
-    return p.map(p.noise(index), 0, 1, 0, p.letW*50);
-  }
-
-  p.draw = function() {
-    p.background(51);
-    p.t++;
-
-    for (var i = 0; i * p.sep < p.canvasWidth; i++) {
-      for (var j = 0; j * p.lineHeight < p.canvasHeight; j++) {
-        p.x = i * p.sep;
-        p.y = j * p.lineHeight;
-        p.noFill()
-        p.stroke('#fff')
-    
-        p.curve (
-          p.createNoise(p.t/1000),
-          p.createNoise(j * p.t/3000) + p.y,
-          p.x,
-          p.y + p.lineHeight,
-          p.x + p.cos(p.t/40)*80,
-          p.y - p.sin(p.t/30)*50,
-          p.createNoise(j++) + p.y,
-          p.createNoise(j++) + p.y
-        )
-      }
+      curve (
+        createNoise(i) + x,
+        createNoise(i * 300) + y,
+        x,
+        y + lineHeight * 2,
+        x - 16 * j / 24,
+        y + 40,
+        createNoise(j++ / 20) + x,
+        createNoise(j++) + y
+      )
     }
   }
+  noLoop();
+  // saveCanvas();
 }
-
-
-var myp51 = new p5(sketch1);
-var myp51 = new p5(sketch2);
 
 
---
+++ sketch2.js
@@ -0,0 +1,108 @@
+var sketch1 = function(p) {
+  
+  p.canvasWidth = 1600;
+  p.canvasHeight = 800;
+  p.lineHeight = 24;
+  p.sep = 16;
+  p.letW = 16;
+  p.t = 0;
+
+  p.setup = function() {
+    p.createCanvas(p.canvasWidth, p.canvasHeight);
+  }
+
+
+  p.createNoise = function(index) {
+    return 100/p.noise(index) - 10;
+  }
+
+  p.draw = function() {
+    p.background(51);
+    p.t++;
+
+    for (i = 0, y = 0; i * p.sep < p.canvasWidth; i++) {
+      for (j = 0; j * p.lineHeight < p.canvasHeight; j++) {
+        //separation of words
+        if (p.random() > 0.3) {
+          p.x = i * p.sep;
+        } else {
+          p.x = i * p.sep + 80;
+        }
+         
+        p.y = j * p.lineHeight;
+        p.noFill();
+        p.stroke('#fff');
+
+        // if (p.random()<0.005) {
+        //   i += 10 * p.random();
+        // }
+    
+        p.curve (
+          p.createNoise(i) + p.x,
+          p.createNoise(i * 300) + p.y,
+          p.x,
+          p.y + p.lineHeight * 2,
+          p.x - 16 * j / 24,
+          p.y + 40,
+          p.createNoise(j++ / 20) + p.x,
+          p.createNoise(j++) + p.y
+        )
+      }
+    }
+    p.noLoop();
+  }
+}
+
+var sketch2 = function(p) {
+  p.sep = 80;
+  p.canvasWidth = 1600;
+  p.canvasHeight = 800;
+  p.lineHeight = 30;
+  p.letW = 15;
+  p.t = 0;
+
+  p.setup = function() {
+    p.createCanvas(p.canvasWidth, p.canvasHeight);
+    // p.createLoop({duration:15,gif:true})
+  }
+
+
+  p.createNoise = function(index) {
+    return p.map(p.noise(index), 0, 1, 0, p.letW*50);
+  }
+
+  p.mousePressed() = function() {
+    redraw();
+  }
+
+  p.draw = function() {
+    p.background(51);
+    p.t++;
+
+    for (var i = 0; i * p.sep < p.canvasWidth; i++) {
+      for (var j = 0; j * p.lineHeight < p.canvasHeight; j++) {
+        p.x = i * p.sep;
+        p.y = j * p.lineHeight;
+        p.noFill()
+        p.stroke('#fff')
+    
+        p.curve (
+          p.createNoise(p.t/1000),
+          p.createNoise(j * p.t/3000) + p.y,
+          p.x,
+          p.y + p.lineHeight,
+          p.x + p.cos(p.t/40)*80,
+          p.y - p.sin(p.t/30)*50,
+          p.createNoise(j++) + p.y,
+          p.createNoise(j++) + p.y
+        )
+      }
+    }
+  }
+}
+
+
+var myp51 = new p5(sketch1);
+// var myp51 = new p5(sketch2);
+
+
